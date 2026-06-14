252a5b0985e2417e0a3e4c0527c1e02695b300fe
NoReply Amazon <no-reply@amazon.com>
2026-06-14 18:54:28 +0000

    DB transformation: Upload transformed code

--- src/Northwind.Web/Models/OrderDetail.cs
+++ src/Northwind.Web/Models/OrderDetail.cs
@@ -1,14 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Northwind.Web.Models;
 
+[Table("orderdetails", Schema = "northwind_dbo")]
 public class OrderDetail
 {
+    // Composite primary key (OrderID + ProductID) is configured via HasKey() in DbContext
+    [Column("orderid")]
     public int OrderID { get; set; }
+
+    [Column("productid")]
     public int ProductID { get; set; }
+
+    [Column("unitprice")]
     public decimal UnitPrice { get; set; }
+
+    [Column("quantity")]
     public short Quantity { get; set; }
+
+    [Column("discount")]
     public float Discount { get; set; }
 
-    // Computed property (should get [NotMapped])
+    // Computed property
+    [NotMapped]
     public decimal LineTotal => UnitPrice * Quantity * (1 - (decimal)Discount);
 
     // Navigation properties
